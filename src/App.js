42d2ac4ac67ef6134556d3994debb4e6b43e28d4
Idan Dardikman <idandardikman@Idans-MacBook-Pro.local>
2017-12-29 19:42:22 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout} from 'antd';
+import SideBar from './components/SideBar'
+import ContactsTable from './components/ContactsTable'
+
+const { Header, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <SideBar/>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Welcome to LandLord
+            <ContactsTable/>
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default App;
