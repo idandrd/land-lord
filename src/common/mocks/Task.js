792a3ff6fe9fb1d680083537a288e5e7244c7db5
idan <idandrd@gmail.com>
2018-07-09 20:13:17 +0300

    added task card menu

--- src/common/mocks/Task.js
+++ src/common/mocks/Task.js
@@ -1,8 +1,20 @@
 import { TaskStatus, TaskType } from "../constants";
+import moment from "moment";
 
 export const RegularCheckTaskA = {
+  id: "54321",
   contractId: "abc123",
   type: TaskType.RegularCheck,
   status: TaskStatus.Active,
-  alertDate: "2018-08-20"
+  dueDate: "2018-08-20"
+};
+
+export const TaskCardA = {
+  id: "54321",
+  title: "Check Deposit",
+  dueDate: moment(),
+  imgPath: "https://previews.123rf.com/images/cowpland/cowpland1411/cowpland141100049/33356139-bank-check-icon-flat-design-with-long-shadows-.jpg",
+  tenantName: "Idan & Noa",
+  unitAddress: "Netzah-Israel 11, Tel-Aviv",
+  contractId: "12345"
 };
