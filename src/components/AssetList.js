9fcbcd37f99ff433746e48f64d6b617b838c8a21
idan <idandrd@gmail.com>
2018-11-24 01:37:23 +0200

    wip

--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Table } from "antd";
+import { uniq } from "lodash";
 
 const strings = {
   nameColumn: "שם",
+  addressColumn: "כתובת",
+  cityColumn: "עיר",
+  ownerColumn: "בעלים",
   typeColumn: "שימוש",
+  floorsColumn: "קומות",
+  yearColumn: "שנת הקמה",
+  unitsColumn: "יחידות",
   commentsColumn: "הערות"
 };
 
@@ -15,13 +22,38 @@ const columns = [
     sorter: (a, b) => a.name.length - b.name.length
   },
   {
-    title: strings.typeColumn,
-    dataIndex: "type",
+    title: strings.cityColumn,
+    dataIndex: "city",
     sorter: (a, b) => a.type.length - b.type.length
   },
   {
-    title: strings.commentsColumn,
-    dataIndex: "comments",
+    title: strings.addressColumn,
+    dataIndex: "address",
+    sorter: (a, b) => a.comments.length - b.comments.length
+  },
+  {
+    title: strings.floorsColumn,
+    dataIndex: "floors",
+    sorter: (a, b) => a.comments.length - b.comments.length
+  },
+  {
+    title: strings.yearColumn,
+    dataIndex: "year",
+    sorter: (a, b) => a.comments.length - b.comments.length
+  },
+  {
+    title: strings.typeColumn,
+    dataIndex: "type",
+    sorter: (a, b) => a.comments.length - b.comments.length
+  },
+  {
+    title: strings.unitsColumn,
+    dataIndex: "unitsCount",
+    sorter: (a, b) => a.comments.length - b.comments.length
+  },
+  {
+    title: strings.ownerColumn,
+    dataIndex: "owner",
     sorter: (a, b) => a.comments.length - b.comments.length
   }
 ];
@@ -29,6 +61,9 @@ const columns = [
 const onChange = (pagination, filters, sorter) => {
   console.log("params", pagination, filters, sorter);
 };
+
+const mergeUnitField = (asset, fieldName) =>
+  uniq(asset.units.map(unit => unit[fieldName]).filter(a => a)).join(", ");
 
 export class AssetList extends React.Component {
   render() {
@@ -38,12 +73,13 @@ export class AssetList extends React.Component {
         dataSource={
           assets
             ? assets.map((asset, i) => ({
+                ...asset,
                 key: i,
-                name: asset.name,
-                type: asset.type,
-                comments: asset.comments
+                owner: mergeUnitField(asset, "owner"),
+                type: mergeUnitField(asset, "type"),
+                unitsCount: asset.units.length
               }))
-            : [{ name: "idan", type: "man", comments: "hello", key: "1" }]
+            : []
         }
         columns={columns}
         onChange={onChange}
