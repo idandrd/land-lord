421971f13eb48b9f4e47e9b9c92794c53aa6cdef
idan <idandrd@gmail.com>
2019-03-30 21:11:11 +0300

    Update Tasks.tsx

--- src/components/views/Tasks.tsx
+++ src/components/views/Tasks.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Card, Icon, Avatar, Menu, Dropdown, message } from "antd";
+import { Card, Icon, Avatar, Menu, Dropdown, Divider, message } from "antd";
 
 import { PopulatedTask, BaseTask, TaskType } from "../../types";
 import { firebaseService } from "../../service";
 import { ContentFrame } from "../ContentFrame";
 const { Meta } = Card;
+
+interface PopulatedDateTask extends PopulatedTask {
+  deadlineDate: Date;
+}
 
 export class Tasks extends React.Component<{ tasks: PopulatedTask[] }> {
   handleTaskDone(task: PopulatedTask) {
@@ -19,6 +23,11 @@ export class Tasks extends React.Component<{ tasks: PopulatedTask[] }> {
   }
 
   render() {
+    const tasks: PopulatedDateTask[] = [...this.props.tasks]
+      .filter(task => task.status === "active")
+      .map(task => ({ ...task, deadlineDate: new Date(task.deadline) }))
+      .sort(tasksSortCompare);
+
     return (
       <ContentFrame firstNavText={""}>
         <div
@@ -28,39 +37,48 @@ export class Tasks extends React.Component<{ tasks: PopulatedTask[] }> {
             flexDirection: "column"
           }}
         >
-          {this.props.tasks
-            .filter(task => task.status === "active")
-            .map(task => (
-              <Card
-                key={task.id}
-                style={{ direction: "rtl", width: 400, marginBottom: 10 }}
-                hoverable
-                actions={[
-                  <Dropdown overlay={SnoozeMenu} trigger={["click"]}>
-                    <Icon type="clock-circle-o" />
-                  </Dropdown>,
-                  <Icon
-                    type="check"
-                    onClick={() => this.handleTaskDone(task)}
-                  />
-                ]}
-              >
-                <Meta
-                  avatar={<Avatar src={imgPath} />}
-                  title={getTaskTitle(task.taskType)}
-                  description={task.deadline}
-                />
-                <div>{task.contract.tenant.name}</div>
-                <div>{`${task.contract.asset.city} ${
-                  task.contract.asset.address
-                }`}</div>
-              </Card>
-            ))}
+          <Divider>משימות שעבר להן התאריך</Divider>
+          <TasksList tasks={tasks.filter(isPastTask)} />
+          <Divider>משימות לשבוע הקרוב</Divider>
+          <TasksList tasks={tasks.filter(isNextWeekTask)} />
+          <Divider>משימות לחודש הקרוב</Divider>
+          <TasksList tasks={tasks.filter(isNextMonthTask)} />
+          <Divider>משימות לעתיד</Divider>
+          <TasksList tasks={tasks.filter(isFutureTask)} />
+          {/* <TasksList tasks={tasks.filter(task => task.status === "active")} /> */}
         </div>
       </ContentFrame>
     );
   }
 }
+
+const TasksList = (props: { tasks: PopulatedDateTask[] }) => (
+  <div>
+    {props.tasks.map(task => (
+      <Card
+        key={task.id}
+        style={{ direction: "rtl", width: 400, marginBottom: 10 }}
+        hoverable
+        actions={[
+          <Dropdown overlay={SnoozeMenu} trigger={["click"]}>
+            <Icon type="clock-circle-o" />
+          </Dropdown>,
+          <Icon type="check" onClick={() => this.handleTaskDone(task)} />
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src={imgPath} />}
+          title={getTaskTitle(task.taskType)}
+          description={task.deadline}
+        />
+        <div>{task.contract.tenant.name}</div>
+        <div>{`${task.contract.asset.city} ${
+          task.contract.asset.address
+        }`}</div>
+      </Card>
+    ))}
+  </div>
+);
 
 const SnoozeMenu = (
   <Menu>
@@ -74,12 +92,45 @@ const SnoozeMenu = (
   </Menu>
 );
 
+const tasksSortCompare = (taskA: PopulatedTask, taskB: PopulatedTask) => {
+  const dateA = new Date(taskA.deadline);
+  const dateB = new Date(taskB.deadline);
+  if (dateA < dateB) {
+    return -1;
+  }
+  if (dateA > dateB) {
+    return 1;
+  }
+  return 0;
+};
+
 const getTaskTitle = (taskType: TaskType) => {
   switch (taskType) {
     case "depositCheck":
       return "להפקיד צ'ק";
   }
 };
+
+const isPastTask = (task: PopulatedDateTask) => task.deadlineDate < new Date();
+const isNextWeekTask = (task: PopulatedDateTask) => {
+  if (isPastTask(task)) {
+    return false;
+  }
+  const nextWeekDate = new Date();
+  nextWeekDate.setDate(nextWeekDate.getDate() + 7);
+  return task.deadlineDate < nextWeekDate;
+};
+const isNextMonthTask = (task: PopulatedDateTask) => {
+  if (isNextWeekTask(task)) {
+    return false;
+  }
+  console.log(task);
+  const nextMonthDate = new Date();
+  nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
+  console.log("nextMonthDate", nextMonthDate);
+  return task.deadlineDate < nextMonthDate;
+};
+const isFutureTask = (task: PopulatedDateTask) => !isNextMonthTask(task);
 
 const imgPath =
   "https://previews.123rf.com/images/cowpland/cowpland1411/cowpland141100049/33356139-bank-check-icon-flat-design-with-long-shadows-.jpg";
