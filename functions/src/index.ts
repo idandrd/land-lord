6331b6160c5290b364d960f9ce10f555e1a6297d
idan <idandrd@gmail.com>
2019-05-01 18:25:36 +0300

    playing around with functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,21 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin
+    .database()
+    .ref("/messages")
+    .push({ original });
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
