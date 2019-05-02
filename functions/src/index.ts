98eccb8f5559e25fe15e370e069ced2ad2aa174b
idan <idandrd@gmail.com>
2019-05-02 17:27:04 +0300

    function now sends mail

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as sgMail from "@sendgrid/mail";
 
 admin.initializeApp();
 
@@ -8,6 +9,7 @@ admin.initializeApp();
 export const addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
+  await sendEmail(original);
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
   const snapshot = await admin
     .database()
@@ -17,9 +19,17 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
   res.redirect(303, snapshot.ref.toString());
 });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+async function sendEmail(title: string) {
+  // using SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  const sendGridApiKey = functions.config().sendgrid.key;
+  sgMail.setApiKey(sendGridApiKey);
+  const msg = {
+    to: "idandrd@gmail.com",
+    from: "LandLord@landlord.com",
+    subject: `${title} LandLord Daily Tasks`,
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>You have some tasks you need to do today!</strong>"
+  };
+  await sgMail.send(msg);
+}
