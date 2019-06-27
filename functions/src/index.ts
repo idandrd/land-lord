b4d293fe37d6f49cc7bf03d235774bf9cadf52e3
idan <idandrd@gmail.com>
2019-06-27 14:31:36 +0300

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,17 +19,89 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
   res.redirect(303, snapshot.ref.toString());
 });
 
+export const dailyMail = functions.pubsub
+  .schedule("every 24 hours")
+  .onRun(async context => {
+    const date = new Date();
+    const weekDay = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ][date.getDay()];
+    await sendEmail(weekDay);
+  });
+
 async function sendEmail(title: string) {
   // using SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   const sendGridApiKey = functions.config().sendgrid.key;
   sgMail.setApiKey(sendGridApiKey);
+  const tasks = await getTasks();
   const msg = {
     to: "idandrd@gmail.com",
     from: "LandLord@landlord.com",
     subject: `${title} LandLord Daily Tasks`,
     text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>You have some tasks you need to do today!</strong>"
+    html: `<strong>You have some tasks you need to do today!</strong>${getTasksHtml(
+      tasks
+    )}`
   };
   await sgMail.send(msg);
+}
+
+async function getTasks(): Promise<BaseTask[]> {
+  const allTasks: BaseTask[] = [];
+  const snapshot = await admin
+    .firestore()
+    .collection("dev")
+    .doc("root")
+    .collection("cases")
+    .doc("amir123")
+    .collection("tasks")
+    .get();
+  snapshot.forEach(doc => allTasks.push(doc.data() as BaseTask));
+  const filteredTasks = allTasks.filter(
+    task => isToday(new Date(task.deadline)) && task.status === "active"
+  );
+  return filteredTasks;
+}
+
+function getTasksHtml(tasks: BaseTask[]): string {
+  const htmlTasks = tasks.map(task => {
+    const title = `<h2>${getTaskTitle(task)}</h2>`;
+    const deadline = `<p><strong>Date: </strong>${task.deadline}</p>`;
+    return title + deadline;
+  });
+  return `<div>${htmlTasks.join("")}</div>`;
+}
+
+function isToday(someDate: Date) {
+  const today = new Date();
+  return (
+    someDate.getDate() === today.getDate() &&
+    someDate.getMonth() === today.getMonth() &&
+    someDate.getFullYear() === today.getFullYear()
+  );
+}
+
+function getTaskTitle(task: BaseTask): string {
+  switch (task.taskType) {
+    case "depositCheck":
+      return "להפקיד צ'ק";
+    case "outOfChecks":
+      return "הצ'קים עומדים להיגמר! צריך לבקש חדשים";
+    case "endOfContract":
+      return "החוזה יסתיים בעוד 3 חודשים";
+  }
+}
+
+interface BaseTask {
+  contractId: string;
+  taskType: "depositCheck" | "outOfChecks" | "endOfContract";
+  deadline: string;
+  status: "active" | "done" | "snoozed" | "deleted";
 }
