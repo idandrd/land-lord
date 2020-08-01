7d0818d5b5538b5fbb469756313ed57d695bb4c0
idan <idandrd@gmail.com>
2020-08-01 15:21:21 +0300

    Update ContractView.tsx

--- src/components/ContractView.tsx
+++ src/components/ContractView.tsx
@@ -4,9 +4,11 @@ import { FormItem } from "./FormItem";
 import { strings as formStrings } from "./ContractForm";
 import { Link } from "react-router-dom";
 
-import { PopulatedContract } from "../types";
+import { PopulatedContract, Unit } from "../types";
 
-const strings = {};
+const strings = {
+  owner: "בעלים"
+};
 
 const stringFields = [{ fieldKey: "" }];
 
@@ -15,13 +17,20 @@ export class ViewContract extends React.Component<{
 }> {
   render() {
     const { tenant, asset, ...contract } = this.props.contract;
+    const units = getRelevantUnits(asset.units, contract.unitIds);
     return (
       <div style={{ direction: "rtl" }}>
         <FormItem label={formStrings.tenant}>{tenant.name}</FormItem>
         <FormItem label={formStrings.asset}>{`${asset.address}, ${asset.city}`}</FormItem>
         <FormItem label={formStrings.signingDate}>{contract.signingDate}</FormItem>
         <FormItem label={formStrings.startLeaseDate}>{contract.startLeaseDate}</FormItem>
+        <FormItem label={formStrings.unit}>{units.map(unit => unit.name).join()}</FormItem>
       </div>
     );
   }
+}
+
+function getRelevantUnits(units: Unit[], unitIds: string[]) {
+  const unitsWithId = units.map((unit, index) => (unit.id ? {...unit} : {...unit, id: index}))
+  return unitsWithId.filter(unit => unitIds.includes(unit.id as any))
 }
